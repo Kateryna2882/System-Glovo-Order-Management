bfc4f786b5ac7e9d6b85a3e8ed989f42a11471f0
Kateryna2882 <govoryhina82@gmail.com>
2024-01-27 20:21:20 +0200

    Initial commit

---
+++ src/main/java/com/example/systemglovoordermanagement/SystemGlovoOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemglovoordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemGlovoOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemGlovoOrderManagementApplication.class, args);
+	}
+
+}
